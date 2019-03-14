e4237bb15e4a081c1256f2b7927d77b942ad7bba
wandokim <kdw1521@naver.com>
2019-03-14 13:00:10 +0900

    wando

--- src/main/java/sol/desk/wjjst/FindID.java
+++ src/main/java/sol/desk/wjjst/FindID.java
@@ -3,16 +3,26 @@ package sol.desk.wjjst;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class FindID {
-	
+	private final String User_AGENT = "Mozilla/5.0";
 	public String find_id(String name) throws IOException {
-		URL lolApisite = new URL("https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+name+"?api_key=RGAPI-c96828d6-6222-4808-91f4-26ce2998deca");
-		BufferedReader in = new BufferedReader(new InputStreamReader(lolApisite.openStream()));
+		URL lolApisite = new URL("https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+name+"?api_key=RGAPI-42faecd8-589d-4bd7-87a2-f4492f960705");
+		URL obj = lolApisite;
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		//optional default is GET
+		con.setRequestProperty("User-AGENT",User_AGENT );
+		con.getResponseCode();
+		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
 		String lolApiJson = in.readLine();	
 		System.out.println(lolApiJson);
+		
+		
+		
+		
 		return lolApiJson;
 	}
-
+ 
 }
--- src/main/java/sol/desk/wjjst/Rankinfo.java
+++ src/main/java/sol/desk/wjjst/Rankinfo.java
@@ -3,15 +3,23 @@ package sol.desk.wjjst;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class Rankinfo {
-	
+	private final String User_AGENT = "Mozilla/5.0";
 	public String contents(String id) throws IOException {
-		URL lolApisite = new URL("https://kr.api.riotgames.com/lol/league/v4/positions/by-summoner/"+id+"?api_key=RGAPI-c96828d6-6222-4808-91f4-26ce2998deca");
-		BufferedReader in = new BufferedReader(new InputStreamReader(lolApisite.openStream()));
+		URL lolApisite = new URL("https://kr.api.riotgames.com/lol/league/v4/positions/by-summoner/"+id+"?api_key=RGAPI-42faecd8-589d-4bd7-87a2-f4492f960705");
+	
+		URL obj = lolApisite;
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		//optional default is GET
+		con.setRequestProperty("User-AGENT",User_AGENT );
+		con.getResponseCode();
+		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
 		String lolApiJson = in.readLine();	
 		System.out.println(lolApiJson);
+		
 		return lolApiJson;
 	}
 	
