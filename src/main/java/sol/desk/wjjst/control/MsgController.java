3ad55219d72c6d8b704ff2755fe9aea1f6f63b1f
jejykim <jejykim@hotmail.com>
2019-03-21 13:05:03 +0900

    jin

--- src/main/java/sol/desk/wjjst/control/MsgController.java
+++ src/main/java/sol/desk/wjjst/control/MsgController.java
@@ -1,8 +1,12 @@
 package sol.desk.wjjst.control;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import sol.desk.wjjst.dao.MsgDAO;
 
@@ -16,4 +20,12 @@ public class MsgController {
 		this.dao = dao;
 	}
 	
+	
+	@RequestMapping(value = "readMore")
+	public String main(Model model) {
+		List list = dao.getList();
+		
+		model.addAttribute("list", list);
+		return "msg";
+	}
 }
---
+++ src/main/webapp/WEB-INF/views/msg.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+<!-- jstl 값 받아오면서 반복문으로 출력 -->
+
+
+</body>
+</html>
