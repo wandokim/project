f80065a05128ef1cd52fe7c7dba48f2c4583cb28
jejykim <jejykim@hotmail.com>
2019-03-22 14:28:51 +0900

    jin

--- src/main/java/sol/desk/wjjst/KeyValue.java
+++ src/main/java/sol/desk/wjjst/KeyValue.java
@@ -2,7 +2,7 @@ package sol.desk.wjjst;
 
 public class KeyValue {
 	public String keyvalue() {
-		String key = "RGAPI-d285968e-535b-44b7-900b-988494bef185";
+		String key = "RGAPI-e4c071da-a9f8-4b4f-bcbe-de2193f168b6";
 		
 		return key;
 	}
